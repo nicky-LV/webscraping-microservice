fe86b35149b47b40b6ebe82be5dfd74cd33a55a5
nicky-LV <nickystnkvs@outlook.com>
2021-07-19 19:39:03 +0100

    [Refactor] Added slash to url.

--- webscraping_engine/urls.py
+++ webscraping_engine/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ParseUniversities
 
 urlpatterns = [
-    path("parse-universities", ParseUniversities.as_view(), name='parse-universities')
+    path("parse-universities/", ParseUniversities.as_view(), name='parse-universities')
 ]
