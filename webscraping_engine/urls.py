01ffc9a47a4477c48780e740b0ee8a0740680902
nicky-LV <nickystnkvs@outlook.com>
2021-07-22 13:10:24 +0100

    [Backend] Added API endpoint for listing valid universities.

--- webscraping_engine/urls.py
+++ webscraping_engine/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ParseUniversities
+from .views import *
 
 urlpatterns = [
-    path("parse-universities/", ParseUniversities.as_view(), name='parse-universities')
+    path("parse-universities/", ParseUniversities.as_view(), name='parse-universities'),
+    path("valid-universities/", ListValidUniversities.as_view(), name='list-valid-universities')
 ]
