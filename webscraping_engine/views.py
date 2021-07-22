c0f035757509e3f3cfeb91e315e8355ff3f26372
nicky-LV <nickystnkvs@outlook.com>
2021-07-22 13:12:02 +0100

    [API] Added API view for listing valid universities and parsing universities.

--- webscraping_engine/views.py
+++ webscraping_engine/views.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
 
 from typing import List
 
-from .serializers import ParseUniversitiesSerializer
+from .utils import university_is_valid
+from .serializers import ParseUniversitiesSerializer, ValidUniversitySerializer
+from .models import ValidUniversities
 
 
 class ParseUniversities(APIView):
@@ -15,9 +18,42 @@ class ParseUniversities(APIView):
         :param request: object - request object.
         :return: str[] - list of valid universities.
         """
-        university_list: List[str] = request.data
-        serializer = ParseUniversitiesSerializer(university_list, many=True)
+        university_list = request.data
+        serializer = ParseUniversitiesSerializer(data=university_list, many=True)
 
         if serializer.is_valid():
             universities = serializer.validated_data
-            print(universities)
+            valid_universities = [university.name for university in ValidUniversities.objects.all()]
+
+            for idx, university in enumerate(universities):
+                if university in valid_universities:
+                    pass
+
+                else:
+                    university = university["name"]
+                    # Check if the university is on the site, and thus is able to be scraped. 5 retries.
+                    retries = 3
+                    for i in range(retries):
+                        university_name, university_url = university_is_valid(university_name=university,
+                                                                              confirm_cookies=idx == 0)
+
+                        if university_name and university_url:
+                            valid_universities.append({
+                                "name": university_name,
+                                "url": university_url
+                            })
+
+                        else:
+                            # Reduce retry number if university_is_valid returns False.
+                            retries -= 1
+
+            return Response(data=valid_universities, status=status.HTTP_200_OK)
+
+        else:
+            print(serializer.errors)
+            return Response(data="Invalid universities provided", status=status.HTTP_400_BAD_REQUEST)
+
+
+class ListValidUniversities(ListAPIView):
+    queryset = ValidUniversities.objects.all()
+    serializer_class = ValidUniversitySerializer
