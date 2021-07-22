1df37ab1665bf0f0b8a079ead46857db0ecb1727
nicky-LV <nickystnkvs@outlook.com>
2021-07-22 13:10:45 +0100

    [Backend] Connected webscraping_engine API to root urlpatterns.

--- webscraping_microservice/urls.py
+++ webscraping_microservice/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('webscraping_engine.urls'))
+    path('api/', include('webscraping_engine.urls'))
 ]
