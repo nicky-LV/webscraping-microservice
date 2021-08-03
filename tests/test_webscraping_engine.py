c991d1c5ebae95a68c048536251e673495157c42
nicky-LV <nickystnkvs@outlook.com>
2021-08-03 15:57:14 +0100

    [Webscraper tests] Tests for retrieving accommodation names and pictures. Added shutdown method.

--- tests/test_webscraping_engine.py
+++ tests/test_webscraping_engine.py
@@ -3,23 +3,35 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-
-class WebscrapingEngine:
-    """
-    NOTE: Must be ran once dockerized. This will not pass on localhost.
-    """
-    def __init__(self):
-        # Tests startup of webdriver.
-        self.driver = webdriver.Remote("http://selenium:4444/wd/hub", DesiredCapabilities.CHROME)
-
-    def test_shutdown(self):
-        self.driver.quit()
+from webscraping_engine.utils import WebscrapingUtils
 
 
 @pytest.fixture
-def webscraping_engine_instance():
-    return WebscrapingEngine()
+def engine():
+    return WebscrapingUtils()
 
 
-def test_shutdown(webscraping_engine_instance):
-    webscraping_engine_instance.test_shutdown()
+def test_shutdown(engine) -> None:
+    engine.quit()
+
+
+def test_scraping_accommodation_names(engine):
+    accommodation_urls = engine.get_accommodation_urls(
+        university_url="https://www.studentcrowd.com/university-l1003919-s1008319-the_university_of_liverpool-liverpool")
+
+    for accomm_url in accommodation_urls:
+        # Navigate to accommodation page
+        engine.selenium_driver.get(accomm_url)
+
+        # Retrieve accommodation name
+        accomm_name = engine.get_accommodation_name()
+        if accomm_name is None:
+            raise TypeError("accomm_name is None")
+
+
+def test_scraping_accommodation_picture(engine):
+    test_accommodation_url = \
+        "https://www.studentcrowd.com/hall-l1005196-s1044625-rees-hall-university_of_portsmouth-portsmouth#booking"
+    engine.get(url=test_accommodation_url)
+    src = engine.get_accommodation_picture()
+    assert src == "https://media.studentcrowd.net/w426-h284-q70-cfill/20170905-1044625.jpg"
